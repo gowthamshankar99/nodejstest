95b11324e111fbbfd3923bd787d27ddd948949dd
gowthamshankar99 <Gowtham.Shankar@thehartford.com>
2021-05-30 18:37:10 -0400

    appspec

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ app.get("/", function (req, res) {
 
 app.listen(3000);
 // test changes to see if code build updates
+
+//$ sudo apt-get -y install curl
+//$ curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
+//$ sudo apt-get -y install nodejs
+//
+//
