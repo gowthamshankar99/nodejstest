4144430bd7d41fe18bdb30aed837b245a560388c
gowthamshankar99 <Gowtham.Shankar@thehartford.com>
2021-05-30 19:29:39 -0400

    uploads

--- application_start.sh
+++ application_start.sh
@@ -1,2 +1,6 @@
+npm install
+mv /home/ec2-user/.nvm/versions/node/v16.2.0/bin/node /usr/bin/
+mv /home/ec2-user/.nvm/versions/node/v16.2.0/bin/npm /usr/bin/
+mv /home/ec2-user/.nvm/versions/node/v16.2.0/bin/npx /usr/bin/
 sudo systemctl daemon-reload
 sudo systemctl start app
