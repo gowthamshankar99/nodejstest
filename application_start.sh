cc9bf47b8fd98e2b1d9cbaaad21ec9303201d92a
gowthamshankar99 <Gowtham.Shankar@thehartford.com>
2021-05-30 20:10:31 -0400

    update application start

--- application_start.sh
+++ application_start.sh
@@ -1,6 +1,3 @@
 npm install
-mv /home/ec2-user/.nvm/versions/node/v16.2.0/bin/node /usr/bin/
-mv /home/ec2-user/.nvm/versions/node/v16.2.0/bin/npm /usr/bin/
-mv /home/ec2-user/.nvm/versions/node/v16.2.0/bin/npx /usr/bin/
 sudo systemctl daemon-reload
 sudo systemctl start app
