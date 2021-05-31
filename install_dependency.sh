c07d262651099960de4047705eac8ee60b0c0918
gowthamshankar99 <Gowtham.Shankar@thehartford.com>
2021-05-30 20:09:52 -0400

    upload

--- install_dependency.sh
+++ install_dependency.sh
@@ -1,3 +1,3 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+sudo yum install -y gcc-c++ make 
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - 
+sudo yum install -y nodejs
